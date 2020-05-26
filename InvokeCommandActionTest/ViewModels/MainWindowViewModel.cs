5e98b4cb3b0fb8210c466ad4d6f95fd1adff4b64
qtbui159 <qtbui159@live.com>
2020-05-26 08:54:29 +0800

    Init

---
+++ InvokeCommandActionTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvokeCommandActionTest.ViewModels
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public bool Idle { get => m_Idle; private set => SetProperty(ref m_Idle, value); }
+        private bool m_Idle = false;
+
+        public DelegateCommand<object> KeyUpCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            KeyUpCommand = new DelegateCommand<object>(KeyUpCommandProc);
+        }
+
+        private void KeyUpCommandProc(object obj)
+        {
+            
+        }
+    }
+}
